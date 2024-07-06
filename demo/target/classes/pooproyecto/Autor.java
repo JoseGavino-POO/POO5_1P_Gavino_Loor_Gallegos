a731a4e4cae6f834649bdde3953b9408d770eb52
JoseGavino-POO <jagavino@espol.edu.ec>
2024-07-06 18:56:14 -0500

    Autor getters y setter

--- demo/target/classes/pooproyecto/Autor.java
+++ demo/target/classes/pooproyecto/Autor.java
@@ -2,10 +2,28 @@ public class Autor extends Usuario{
   private String institucion;
   private String campoInvestigacion;
   private String correo;
-  public Autor(String institucion,String campoInvestigacion,String correo,String nombre,String apellido,String correo){
-    super(nombre,apeelido,correo);
+  public Autor(String institucion,String campoInvestigacion,String correo,String nombre,String apellido){
+    super(nombre,apellido);
     this.institucion = institucion;
     this.campoInvestigacion = campoInvestigacion;
+    this.correo = correo;
+  }
+  public String getInstitucion(){
+    return institucion;
+  }
+  public void setInstitucion(String institucion){
+    this.institucion = institucion;    
+  }
+  public String getCampo(){
+    return campoInvestigacion;
+  }
+  public void setCampo(String campo){
+    this.campoInvestigacion = campo;    
+  }
+  public String getCorreo(){
+    return correo;
+  }
+  public void setCorreo(String correo){
     this.correo = correo;
   }
 }
