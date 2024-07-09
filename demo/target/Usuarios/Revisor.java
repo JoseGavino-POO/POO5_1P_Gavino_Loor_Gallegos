0d7d42bc15072832db0829b5fe1c0c1bc09703ff
Jose Andres Gaviño Villacis <jagavino@espol.edu.ec>
2024-07-08 20:19:12 -0500

    Clase Revisor

---
+++ demo/target/Usuarios/Revisor.java
@@ -0,0 +1,34 @@
+public class Revisor extends Usuario {
+    private String especialidad;
+    private int numeroArticulosRevisados;
+    
+    public Revisor(String usuario, String contra,String nombre, String apellido, String correo, String especialidad, int numeroArticulosRevisados) {
+    super(usuario, contra,nombre,apellido,correo);
+    this.especialidad = especialidad;
+    this.numeroArticulosRevisados = numeroArticulosRevisados;
+  }
+    
+    public String getEspecialidad(){
+    return especialidad;
+    }
+    public void setEspecialidad(String especialidad){
+    this.especialidad = especialidad;
+    }
+    public int getNarticulosRevisados(){
+    return numeroArticulosRevisados;
+    }
+    public void setNumeroArticulosRevisados(int articulosrevisados){
+    this.numeroArticulosRevisados = articulosrevisados;
+    }
+    @Override
+    public String generarCorreo(){
+    String correo = "Correo revisor";
+            return correo;
+    }
+    
+    @Override
+    public void decidirSobreArticulo(){
+        System.out.println("decision revisor");
+    }
+    
+}
